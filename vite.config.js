34d479dacd71065fff4198a68b553e73ec61dc91
Yasir Hussain <yasirbasumo@gmail.com>
2026-04-02 21:13:25 +0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+<<<<<<< HEAD
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+=======
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+})
+
+>>>>>>> 00c39f5 (calculator update)
